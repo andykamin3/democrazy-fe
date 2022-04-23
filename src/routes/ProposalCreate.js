a29c66929b76b94b3d91711769639bc5b836d938
Barış Bayazıt <bayazitbaris@gmail.com>
2022-04-24 01:33:37 +0200

    Pushhhhhh

--- src/routes/ProposalCreate.js
+++ src/routes/ProposalCreate.js
@@ -24,7 +24,6 @@ export function ProposalCreate() {
       })
       .then(function (response) {
         console.log(response);
-        alert("Done");
       })
       .catch(function (error) {
         console.log(error);
