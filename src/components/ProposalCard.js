e91d1f29658825cbcc32a0fdf01c914528aae1cd
Barış Bayazıt <bayazitbaris@gmail.com>
2022-04-23 23:41:00 +0200

    Git conflicts

--- src/components/ProposalCard.js
+++ src/components/ProposalCard.js
@@ -15,40 +15,76 @@ export default function ProposalCard({ proposal }) {
   const lbl = new Date(props * 1000).toDateString();
   return (
     <Box sx={{ minWidth: 275 }}>
-      <Card variant="confined" sx={{
-        borderRadius: 5,
-        border: "1px solid white",
-        backgroundColor: "background.default",
-        padding: "5px",
-        paddingX: "8px"
-      }}>
+      <Card
+        variant="confined"
+        sx={{
+          borderRadius: 5,
+          border: "1px solid white",
+          backgroundColor: "background.default",
+          padding: "5px",
+          paddingX: "8px",
+        }}
+      >
         <React.Fragment>
-        <CardContent>
-          <Typography sx={{ fontSize: 14 }} color="text.primary" gutterBottom>
-            By <Chip variant="outlined" color="primary" size="small" icon={<FaceIcon />} label={author.substring(0,10)+"..."} component={"span"} />
+          <CardContent>
+            <Typography sx={{ fontSize: 14 }} color="text.primary" gutterBottom>
+              By{" "}
+              <Chip
+                variant="outlined"
+                color="primary"
+                size="small"
+                icon={<FaceIcon />}
+                label={proposal.author.substring(0, 10) + "..."}
+                component={"span"}
+              />
+            </Typography>
+            <Typography sx={{ fontWeight: 900, marginTop: 1.5 }} variant="h5">
+              {proposal.title}
+            </Typography>
 
-          </Typography>
-          <Typography sx={{ fontWeight: 900, marginTop: 1.5 }} variant="h5">
-            {title}
-          </Typography>
-
-          <Typography variant="body2">
-            {description.substring(0,140)}
-            {description.length>140?"...":""}
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            Status
-          </Typography>
-          <Stack direction={"row"} spacing={1} className={"dao-title-container"}>
-            {status.result !== undefined && <Chip sx={{ height: 35, borderRadius: 50 }} color={status.result ? "success":"error"}   label={status?.result ? "Aye" : "Nay"} component={"span"} /> }
-            <Chip sx={{ height: 35, paddingX: 1}} variant="outlined" color={new Date(status?.date)>Date.now() ? "secondary":"error"} size="small" icon={<CalendarMonthIcon />} label={status?.date} component={"span"} />
-
-          </Stack>
-        </CardContent>
-        <CardActions>
-          <Button size="small"><Link component={RouterLink} to={"proposal/"+id}>Learn More</Link></Button>
-        </CardActions>
-      </React.Fragment>
+            <Typography variant="body2">
+              {proposal.description.substring(0, 140)}
+              {proposal.description.length > 140 ? "..." : ""}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              Status
+            </Typography>
+            <Stack
+              direction={"row"}
+              spacing={1}
+              className={"dao-title-container"}
+            >
+              {proposal.status.result !== undefined && (
+                <Chip
+                  sx={{ height: 35, borderRadius: 50 }}
+                  color={proposal.status.result ? "success" : "error"}
+                  label={proposal.status?.result ? "Aye" : "Nay"}
+                  component={"span"}
+                />
+              )}
+              <Chip
+                sx={{ height: 35, paddingX: 1 }}
+                variant="outlined"
+                color={
+                  new Date(proposal.status?.date) > Date.now()
+                    ? "secondary"
+                    : "error"
+                }
+                size="small"
+                icon={<CalendarMonthIcon />}
+                label={lbl}
+                component={"span"}
+              />
+            </Stack>
+          </CardContent>
+          <CardActions>
+            <Button size="small">
+              <Link component={RouterLink} to={"proposal/" + proposal.id}>
+                Learn More
+              </Link>
+            </Button>
+          </CardActions>
+        </React.Fragment>
       </Card>
     </Box>
   );
