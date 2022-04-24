dd5f66231f9eb982d3dc2d8151fa9f99feb7777a
Roshan Palakkal <roshan@surfer-172-29-3-241-hotspot.internet-for-guests.com>
2022-04-24 07:13:27 +0200

    added ipns update on snapshot upload

--- src/routes/ProposalCreate.js
+++ src/routes/ProposalCreate.js
@@ -1,10 +1,11 @@
-import {Paper, Stack, TextField} from "@mui/material";
+import { Paper, Stack, TextField } from "@mui/material";
 import Button from "@mui/material/Button";
-import {Controller, useForm} from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import axios from "axios";
-import {useParams} from "react-router-dom";
-import {useWeb3React} from "@web3-react/core";
-import {generate_pk} from "../utilities/CryptographicGoodies";
+import { useParams } from "react-router-dom";
+import { useWeb3React } from "@web3-react/core";
+import { generate_pk } from "../utilities/CryptographicGoodies";
+import * as ipns from 'ipns';
 
 export function ProposalCreate() {
   const { daoId } = useParams();
@@ -12,25 +13,35 @@ export function ProposalCreate() {
   const { handleSubmit, control } = useForm();
   const { account, active } = useWeb3React();
 
-  const onSubmit = async data => {
+  const onSubmit = async (data) => {
     const sign = await window.ethereum.request({
       method: "personal_sign",
       params: [`Creating a proposal for ${daoId}`, account],
     });
     let publicKey = generate_pk(String(sign)).toString();
-    axios
+    let proposalId;
+    await axios
       .post("https://dc-backend-rpal.vercel.app/addproposal", {
         ...data,
         daoId,
         publicKey,
-        author: account
+        author: account,
       })
       .then(function (response) {
         console.log(response);
+        proposalId = response.data.id;
       })
       .catch(function (error) {
         console.log(error);
       });
+    let ipfsCid;
+    await axios
+      .post("https://dc-backend-rpal.vercel.app/takeSnapshot/" + proposalId)
+      .then((res) => {
+        ipfsCid = res.data.cid;
+      });
+    const entryData = await ipns.create(String(sign), ipfsCid, 1, 60000)
+    
   };
   /**
    * title
